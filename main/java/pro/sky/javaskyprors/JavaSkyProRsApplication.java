e091be06297b0fd1a500188854241a2c579ce876
ranss@rambler.ru <ranss@rambler.ru>
2023-05-05 21:17:09 +0300

    Начальный коммит

---
+++ main/java/pro/sky/javaskyprors/JavaSkyProRsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.javaskyprors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSkyProRsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSkyProRsApplication.class, args);
+	}
+
+}
